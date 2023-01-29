560ebfe41a6b6717c34515853910aa1bdcfe7086
Ricardo Sandoval Pérez <rsandovalp@uoc.edu>
2023-01-29 21:50:17 +0100

    Adding examples 02

--- src/main/java/Section_1/ArrayTask_06.java
+++ src/main/java/Section_1/ArrayTask_06.java
@@ -4,18 +4,30 @@ import java.util.Scanner;
 
 public class ArrayTask_06 {
     public static void main(String[] args) {
-        int [] numbers = new int[5];
+        int[] numbers = new int[5];
         Scanner in = new Scanner(System.in);
         int num, numValues = 0;
-        boolean valid = true;
-
-        while (numValues == 5) {
-            do{
-                System.out.println("Insert a new and uniq number: ");
+        boolean valid;
+        while (numValues < numbers.length) {
+            do {
+                valid = true;
+                System.out.println("Insert a new and unique number: ");
                 num = in.nextInt();
-            }while (numValues == 5);
+                for (int i = 0; i < numValues; i++) {
+                    if (num == numbers[i]) {
+                        System.out.println("The number already exists");
+                        valid = false;
+                        break;
+                    }
+                }
+            } while (!valid);
+            numbers[numValues] = num;
+            numValues++;
         }
-
-
+        for (int number : numbers) {
+            System.out.print(number + ", ");
+        }
+        in.close();
     }
+
 }
